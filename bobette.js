44213f90e6706cf24d315c42297440dc6092c42e
JackBrasesco <jacbras@nuevaschool.org>
2016-10-17 15:14:47 -0700

    Classadoo autocommit

---
+++ bobette.js
@@ -0,0 +1,17 @@
+var output = $("#output");
+var input = $("#input");
+
+var inc = 0;
+function talking() {
+  inc = inc + 1;
+}
+
+input.keydown(function(e) {
+  talking()
+  if (e.keyCode == 13) {
+  output.html("um. . . I have a boyfriend");
+    input.val("")
+  }})
+   
+
+console.log(inc);

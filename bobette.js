1fd066013e0bea1796a9af556b3d73b9e13da7b5
JackBrasesco <jacbras@nuevaschool.org>
2016-10-17 14:57:23 -0700

    Classadoo autocommit

--- bobette.js
+++ bobette.js
@@ -4,23 +4,26 @@ var input = $("#input");
 input.keydown(function(e) {
   talking()
   if (e.keyCode == 13) {
-    if (inc == 0) {
+    if (inc == 1) {
   output.html("um. . . I have a boyfriend");
     }
-    else if (inc == 1) {
+    else if (inc == 2) {
       output.html("seriously, don't talk to me")
     }
-    else if (inc == 2) {
+    else if (inc == 3) {
       output.html("bob is going to kill you if you keep talking to me");
     }
-    else if (inc > 2) {
+    else if (inc > 3) {
       output.html("you should talk to bob")
       alert("seiously, talk to bob at 'jackbrasesco.github.io/bob'")
     }
 }
 })
 
-inc = 0
+console.log(inc)
+
+
+var inc = 0
 function talking() {
   inc = inc + 1
 }
